3f63dd6a338449b1ca2c22837eab4d7234b1ea36
forhackweps-ship-it <forhackweps@gmail.com>
2026-07-24 07:07:57 +0300

    Refactor deployCommands function for clarity

--- src/deployCommands.ts
+++ src/deployCommands.ts
@@ -8,21 +8,20 @@ const commands = [
   }
 ];
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+const rest = new REST({version:"10"})
+.setToken(process.env.TOKEN!);
 
-async function deployCommands() {
-  try {
-    console.log("Started refreshing application commands...");
+async function deploy(){
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID!),
-      { body: commands }
-    );
+await rest.put(
+Routes.applicationCommands(process.env.CLIENT_ID!),
+{
+body: commands
+}
+);
 
-    console.log("Successfully reloaded application commands.");
-  } catch (error) {
-    console.error(error);
-  }
+console.log("Commands deployed");
+
 }
 
-deployCommands();
+deploy();
